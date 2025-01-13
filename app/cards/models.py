f30c8a5d167317cb7e1347329a6f26ed6d31dc46
gija <sany.zombimana.2010@gmail.com>
2025-01-13 21:02:23 +0300

    refactor models and db add log model

--- app/cards/models.py
+++ app/cards/models.py
@@ -1,16 +1,44 @@
-from sqlalchemy import ForeignKey, text, Text
+from sqlalchemy import ForeignKey, text, Text, Column, Integer, String, DateTime, Date, func
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import relationship, Mapped, mapped_column
-from app.db import Base, str_uniq, int_pk, str_null_true, created_at, updated_at
-from datetime import date
+from app.db import Base
+from datetime import datetime
+
+
+# class CardOrm(Base):
+#     __tablename__ = "Cards"
+#
+#     id: Mapped[int_pk]
+#     author: Mapped[str]
+#     created_at: Mapped[created_at]
+#     updated_at: Mapped[updated_at]
+#     title: Mapped[str]
+#     content: Mapped[str]
+#     date_review: Mapped[date]
+#
+#
+# class Log(Base):
+#     __tablename__ = "Logs"
+#
+#     id: Mapped[int_pk]
+#     log_content: Mapped[dict] = mapped_column(JSONB, nullable=False)
 
 
 class CardOrm(Base):
     __tablename__ = "Cards"
 
-    id: Mapped[int_pk]
-    author: Mapped[str]
-    created_at: Mapped[created_at]
-    updated_at: Mapped[updated_at]
-    title: Mapped[str]
-    content: Mapped[str]
-    date_review: Mapped[date]
+    id = Column(Integer, primary_key=True)
+    author = Column(String, nullable=False)
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), onupdate=datetime.now)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    date_review = Column(Date)
+
+
+class Log(Base):
+    __tablename__ = "Logs"
+
+    id = Column(Integer, primary_key=True)
+    card_id = Column(ForeignKey("Cards.id"), nullable=False)
+    log_content = Column(JSONB, nullable=False)
--- app/db.py
+++ app/db.py
@@ -4,6 +4,8 @@ from typing import Annotated
 from sqlalchemy import func
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
+from sqlalchemy.sql.annotation import Annotated
+from sqlalchemy.dialects.postgresql import JSONB
 
 from app.config import get_db_url
 
@@ -13,11 +15,11 @@ engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 # настройка аннотаций
-int_pk = Annotated[int, mapped_column(primary_key=True)]
-created_at = Annotated[datetime, mapped_column(server_default=func.now())]
-updated_at = Annotated[datetime, mapped_column(server_default=func.now(), onupdate=datetime.now)]
-str_uniq = Annotated[str, mapped_column(unique=True, nullable=False)]
-str_null_true = Annotated[str, mapped_column(nullable=True)]
+# int_pk = Annotated[int, mapped_column(primary_key=True)]
+# created_at = Annotated[datetime, mapped_column(server_default=func.now())]
+# updated_at = Annotated[datetime, mapped_column(server_default=func.now(), onupdate=datetime.now)]
+# str_uniq = Annotated[str, mapped_column(unique=True, nullable=False)]
+# str_null_true = Annotated[str, mapped_column(nullable=True)]
 
 
 class Base(AsyncAttrs, DeclarativeBase):
